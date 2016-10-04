ebf1f60d0c4a3fd78373d0f8eee749be128c8ee0
Marty McLaughlin <martymclaugh@gmail.com>
2016-10-04 10:26:42 -0700

    add phone number to user migration

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160915173640) do
     t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "phone"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

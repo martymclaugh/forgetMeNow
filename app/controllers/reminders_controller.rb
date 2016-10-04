8451ea104a35c33a252f387e476d99696e31732b
Marty McLaughlin <martymclaugh@gmail.com>
2016-10-04 16:46:40 -0700

    add jquery ui rails gem to reformat date

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,20 +1,18 @@
 class RemindersController < ApplicationController
   def index
     @reminder = Reminder.new
+    @reminders = Reminder.where(user_id: current_user.id)
+    @reminders_by_date = @reminders.group_by(&:date)
+    p "*" * 100
+    p @reminders_by_date
+    p "*" * 100
   end
 
   def create
-    p params
-    @reminder = Reminder.new(user_id: current_user.id, title: params[:title], message: params[:message], date: params[:date], time: params[:time])
+    @reminder = Reminder.new(user_id: current_user.id, title: params[:reminder][:title], message: params[:reminder][:message], date: params[:reminder][:date], time: params[:reminder][:time])
     if @reminder.save
       redirect_to user_reminders_path
     end
   end
 
-
-  private
-
-  def reminder_params
-    params.require(:reminder).permit(:title, :user_id, :message, :date, :time)
-  end
 end

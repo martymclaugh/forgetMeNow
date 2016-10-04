eb1d2bfef78085d473c956d5d3c1334fb7d0baa8
Marty McLaughlin <martymclaugh@gmail.com>
2016-10-04 13:32:40 -0700

    change redirect from login, define create method for reminders:

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -2,4 +2,19 @@ class RemindersController < ApplicationController
   def index
     @reminder = Reminder.new
   end
+
+  def create
+    p params
+    @reminder = Reminder.new(user_id: current_user.id, title: params[:title], message: params[:message], date: params[:date], time: params[:time])
+    if @reminder.save
+      redirect_to user_reminders_path
+    end
+  end
+
+
+  private
+
+  def reminder_params
+    params.require(:reminder).permit(:title, :user_id, :message, :date, :time)
+  end
 end
